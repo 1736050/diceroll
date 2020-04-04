a7a2dd3c022df8f4cd221dcdda27baecded0a50e
mhamz <1736050@brunel.ac.uk>
2020-04-04 22:50:09 +0100

    finished app

---
+++ app/src/main/java/com/example/dicegame/Main2Activity.java
@@ -0,0 +1,116 @@
+package com.example.dicegame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        PlayIceBreakers();
+
+    }
+    private TextView RandomQuestionButton;
+    static ArrayList<String> arlist = new ArrayList<>();
+
+
+    public void PlayIceBreakers(){
+
+
+        MainActivity ma = new MainActivity();
+        //- 1 to prevent it from going out of bounds as there is a +1 in random number generator
+        int arlistSize = arlist.size() - 1;
+        int number = ma.randomNumGenerator(arlistSize);
+
+
+        RandomQuestionButton = this.findViewById(R.id.RandQText);
+
+        String randomQuestion = arlist.get(number);
+
+
+        RandomQuestionButton.setText(randomQuestion);
+
+
+
+    }
+
+    public ArrayList<String> getList() {
+        return arlist;
+    }
+
+    public void on_button_click(View view){
+        PlayIceBreakers();
+    }
+
+    //when button "add your own question" is clicked
+    public void on_button2_click(View view){
+        Intent intent = new Intent ( Main2Activity.this, Main3Activity.class);
+        startActivity(intent);
+    }
+
+    public void on_homebutton_click(View view){
+        Intent intent = new Intent ( Main2Activity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+    public void QuestionDB(){
+
+        //ArrayList<String> arlist = new ArrayList<String>();
+
+        arlist.add("");
+        arlist.add("If you could go anywhere in the world, where would you go?");
+        arlist.add("If you were stranded on a desert island, what three things would you want to" +
+                " take with you?");
+        arlist.add("If you could eat only one food for the rest of your life, what would that be?");
+        arlist.add("If you won a million dollars, what is the first thing you could buy?");
+        arlist.add("If you could spend the day with one fictional character, who would it be?");
+        arlist.add("If you found a magic lantern and a genie gave you three wishes, what would you" +
+                " wish?");
+
+    }
+
+    public void addtoQuestionDB(String Addme){
+        arlist.add(Addme);
+    }
+
+
+}
+
+
+
+
+
--- app/src/main/java/com/example/dicegame/MainActivity.java
+++ app/src/main/java/com/example/dicegame/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dicegame;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,6 +9,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.Editable;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -57,42 +59,149 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    int count =0;
-public void Random_Number_Generator (View view) {
 
-   // level 1
-    TextView txt = this.findViewById(R.id.TextView);
-    EditText editText = this.findViewById(R.id.textview2);
-    TextView txt2 = this.findViewById(R.id.textview5);
-    Random R = new Random();
-    int rand = 0;
-    while (true){
-        rand = R.nextInt(7);
-        if (rand !=0) break;
+    public void on_button_click(View view){
+
+        randomNumGenerator(6);
+        showRandNum();
+
+        //for result message
+        TextView showResultMessage = this.findViewById(R.id.ResultMessage);
+
+        EditText mEdit = findViewById(R.id.enternumberhere);
+        //Editable guessedText = mEdit.getText();
+
+
+
+        Editable guessedText;
+
+        //to check if input is empty
+        if (!mEdit.getText().toString().equals("")) {
+
+
+            try {
+
+                guessedText = mEdit.getText();
+
+                //for scoring
+                TextView showScoreText = this.findViewById(R.id.ScoreText);
+
+                //for total score
+                totaltries +=1;
+
+                //parsing to int
+                int guessme = Integer.parseInt(guessedText.toString());
+
+                ConstraintError(guessme);
+
+                CompareGuess(guessme, number);
+
+
+            } catch (Exception ex) {
+                Toast.makeText(MainActivity.this, "Error " + ex.toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
+
     }
-    txt.setText(Integer.toString(rand));
+   public void on_button2_click(View view){
+
+        Main2Activity ma2 = new Main2Activity();
+        ma2.QuestionDB();
+
+        Intent intent = new Intent ( MainActivity.this, Main2Activity.class);
+        startActivity(intent);
 
 
-// Level 2
-    int num = Integer.parseInt(editText.getText().toString());
-
-    if ((num < 1 || num > 6)== false) {
     }
-    else
-    {
-        Toast.makeText(this,"Error", Toast.LENGTH_SHORT).show();
+
+
+    //initialising variables
+    private int score;
+    private int totaltries;
+    private int number;
+    static int arlistSize = 6;
+
+    //Editable guessedText;
+    //EditText mEdit = findViewById(R.id.enternumberhere);
+    private TextView showResultMessage;
+    private TextView showconstraintError;
+
+    public int randomNumGenerator(int randMax){
+
+        Random r = new Random();
+        number = r.nextInt(randMax)+1;
+
+
+        return number;
     }
-if (num == rand ){
-    count++;
-    txt2.setText(Integer.toString(count));
-    Toast.makeText(this,"Congratulation", Toast.LENGTH_SHORT).show();
+    public void showRandNum(){
+        TextView tv = this.findViewById(R.id.numbertextView);
+        tv.setText(Integer.toString(number));
+    }
+
+
+    public void CompareGuess(int guessme, int number){
+
+
+        //for result message
+        showResultMessage = this.findViewById(R.id.ResultMessage);
+
+
+        //if entered number is valid
+        if (guessme == number){
+
+            //changes result message
+            showResultMessage.setText("Congratulations you guessed the right number");
+
+            //increments score by 5
+            score += 1;
+
+            Scoring(score, totaltries);
+        }
+        else{
+            //changes result message
+            showResultMessage.setText("You did not guess the right number");
+
+            Scoring(score, totaltries);
+
+        }
+    }
+
+    public void ConstraintError(int guessme){
+
+        //for error messsage
+        showconstraintError =  this.findViewById(R.id.constraintError);
+
+        showconstraintError.setText("Please enter a valid number between 1-6");
+
+        //when entered number is not between 1-6
+        if (guessme < 1 || guessme > 6 ){
+            showconstraintError.setText("Please enter a valid number between 1-6");
+        }
+        else{
+            showconstraintError.setText("");
+        }
+
+    }
+
+    public void Scoring(int score, int totaltries){
+        //for scoring
+        TextView showScoreText = this.findViewById(R.id.ScoreText);
+
+        showScoreText.setText(score + " / " + totaltries);
+    }
+
+    //called when user enters a new icebreaker
+    public void increasearlistSize(){
+        arlistSize += 1;
+    }
+
 
 }
-
-    }
-
-
-    }
 
 
 
